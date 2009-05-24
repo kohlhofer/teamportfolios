71dfc02444fcf59579fbb01bffc4b09242caf194
Tim Diggins <tim@red56.co.uk>
2009-05-24 20:51:19 +0100

    added strapline to project and bio to user

--- db/development_structure.sql
+++ db/development_structure.sql
@@ -1,7 +1,38 @@
-CREATE TABLE `contributors` (
+CREATE TABLE `avatars` (
+  `id` int(11) NOT NULL auto_increment,
+  `user_id` int(11) default NULL,
+  `content_type` varchar(255) default NULL,
+  `filename` varchar(255) default NULL,
+  `thumbnail` varchar(255) default NULL,
+  `size` int(11) default NULL,
+  `width` int(11) default NULL,
+  `height` int(11) default NULL,
+  `parent_id` int(11) default NULL,
+  `created_at` datetime default NULL,
+  `updated_at` datetime default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `contributions` (
   `id` int(11) NOT NULL auto_increment,
   `project_id` int(11) default NULL,
   `user_id` int(11) default NULL,
+  `created_at` datetime default NULL,
+  `updated_at` datetime default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `images` (
+  `id` int(11) NOT NULL auto_increment,
+  `user_id` int(11) default NULL,
+  `content_type` varchar(255) default NULL,
+  `filename` varchar(255) default NULL,
+  `thumbnail` varchar(255) default NULL,
+  `caption` varchar(255) default NULL,
+  `size` int(11) default NULL,
+  `width` int(11) default NULL,
+  `height` int(11) default NULL,
+  `parent_id` int(11) default NULL,
   `created_at` datetime default NULL,
   `updated_at` datetime default NULL,
   PRIMARY KEY  (`id`)
@@ -15,13 +46,24 @@ CREATE TABLE `projects` (
   `homepage` varchar(255) default NULL,
   `created_at` datetime default NULL,
   `updated_at` datetime default NULL,
+  `strapline` varchar(255) default NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `schema_migrations` (
   `version` varchar(255) NOT NULL,
   UNIQUE KEY `unique_schema_migrations` (`version`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `unvalidated_contributors` (
+  `id` int(11) NOT NULL auto_increment,
+  `project_id` int(11) default NULL,
+  `name` varchar(255) default NULL,
+  `email` varchar(255) default NULL,
+  `created_at` datetime default NULL,
+  `updated_at` datetime default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL auto_increment,
@@ -34,6 +76,8 @@ CREATE TABLE `users` (
   `updated_at` datetime default NULL,
   `remember_token` varchar(40) default NULL,
   `remember_token_expires_at` datetime default NULL,
+  `strapline` varchar(255) default NULL,
+  `bio` text,
   PRIMARY KEY  (`id`),
   UNIQUE KEY `index_users_on_login` (`login`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
@@ -45,3 +89,15 @@ INSERT INTO schema_migrations (version) VALUES ('20090323232347');
 INSERT INTO schema_migrations (version) VALUES ('20090324234909');
 
 INSERT INTO schema_migrations (version) VALUES ('20090523112401');
+
+INSERT INTO schema_migrations (version) VALUES ('20090524121133');
+
+INSERT INTO schema_migrations (version) VALUES ('20090524152433');
+
+INSERT INTO schema_migrations (version) VALUES ('20090524164033');
+
+INSERT INTO schema_migrations (version) VALUES ('20090524164220');
+
+INSERT INTO schema_migrations (version) VALUES ('20090524193518');
+
+INSERT INTO schema_migrations (version) VALUES ('20090524193528');
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090524164220) do
+ActiveRecord::Schema.define(:version => 20090524193528) do
 
   create_table "avatars", :force => true do |t|
     t.integer  "user_id"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(:version => 20090524164220) do
     t.string   "homepage"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "strapline"
   end
 
   create_table "unvalidated_contributors", :force => true do |t|
@@ -73,6 +74,7 @@ ActiveRecord::Schema.define(:version => 20090524164220) do
     t.string   "remember_token",            :limit => 40
     t.datetime "remember_token_expires_at"
     t.string   "strapline"
+    t.text     "bio"
   end
 
   add_index "users", ["login"], :name => "index_users_on_login", :unique => true
