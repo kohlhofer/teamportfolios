c8d36e3855cf08b15f8b4b3e679d03ec282b673a
Tim Diggins <tim@red56.co.uk>
2009-05-24 12:30:41 +0100

    some more tests for projects (some breaking)

--- test/integration/projects_test.rb
+++ test/integration/projects_test.rb
@@ -3,33 +3,36 @@ require "#{File.dirname(__FILE__)}/../test_helper"
 class ProjectsTest < ActionController::IntegrationTest
   fixtures :projects, :users
   context "Unlogged in user" do
-    should "not be able to see anything without logging in" do
-      get("projects")
-      assert_response :redirect
-      follow_redirect!
-      assert_has_login_form
-    end
-  end
-  
-  context "Logged in user" do
-    should "be able to see projects page" do
-      login('alex')
-      get("projects")
-      assert_response :success
-      assert_doesnt_have_login_form
-      assert_select 'ul#projects li', :count => 3
-    end
     
     should "be able to see  individual project page" do
-      login('alex')
       get("projects/weewar")
       assert_response :success
       assert_doesnt_have_login_form
       assert_select 'a[href=http://weewar.com]', :count => 1
     end
+
+        should "be able to see projects listing" do
+      get("projects")
+      assert_response :success
+      assert_doesnt_have_login_form
+      assert_select 'ul#projects li', :count => 4
+    end
+
+  end
+  
+  context "Logged in user" do
+    setup do
+      login('alex')
+    end
+    
+    should "be able to see own projects" do
+      get("users/alex")
+      assert_response :success
+      assert_doesnt_have_login_form
+      assert_select 'ul#projects li', :count => 3
+    end
     
     should "be able to create new project page" do
-      login("alex")
       get 'users/alex'
       click_link "new-project-link"
       assert_response_ok
@@ -45,19 +48,50 @@ class ProjectsTest < ActionController::IntegrationTest
       assert_select "ul#contributors" do 
         assert_select"a[href='/users/alex']"
       end
-      
     end
     
-    should "be able to edit collaborating project page" do
-      login('alex')
+  end 
+  
+  
+  context "User contributing to project" do
+    
+    setup do
+      login 'alex'
+      get 'projects/weewar'
+    end
+    
+    should "be able to edit it" do
+      click_link "edit-project-link"
       put_via_redirect "/projects/weewar", :user => { :name => "Fandango" }
       assert_response :success
       assert_doesnt_have_login_form
       assert_select 'h1', :text => /Fandango/
     end
     
-    should "should not be able to edit non-collaborating project page" do
-      login('alex')
+    should "should able to add other contributor" do
+      click_link "add-contributor-link"
+      put_via_redirect "/projects/cleverplugs", :user => { :name => "Golgomesh" }
+      assert_response :success
+      assert_doesnt_have_login_form
+      assert_select 'h1', :text => /Alexander Kohlhofer/
+    end
+  end
+  
+  
+  context "User not contributing to project" do
+    
+    setup do
+      login 'alex'
+      get 'projects/cleverplugs'
+    end
+    
+    should "not be able to edit it" do
+      assert_select 'a#edit-project-link', :count=>0
+      post_via_redirect request.url, :project => {:title => 'GilgameshProj'}
+      assert_response :unauthorized
+    end
+    
+    should "should not be able to add contributor" do
       put_via_redirect "/projects/cleverplugs", :user => { :name => "Golgomesh" }
       assert_response :success
       assert_doesnt_have_login_form
