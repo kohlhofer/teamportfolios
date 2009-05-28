15efb4697b5fdb1a1f7f431ef0cbdb523831465f
Tim Diggins <tim@red56.co.uk>
2009-05-28 07:26:12 +0100

    users fixing

--- test/integration/users_test.rb
+++ test/integration/users_test.rb
@@ -3,26 +3,38 @@ require "#{File.dirname(__FILE__)}/../test_helper"
 class UsersTest < ActionController::IntegrationTest
   fixtures :users
   context "Unlogged in user" do
-    should "not be able to see anything without logging in" do
-      get("users/alex")
-      assert_response :redirect
-      follow_redirect!
-      assert_has_login_form
+    should "be able to see user page without links" do
+      get("users/tim")
+      assert_response :success
+      assert_doesnt_have_login_form
+      assert_select 'h1', :text => /Tim Diggins/
+      assert_have_new_project_link false
     end
   end
   
   context "Logged in user" do
     setup do
-        login('alex')
+      login('alex')
     end
-
-    should "be able to see user page" do
+    
+    should "be able to see user page without links" do
       get("users/tim")
       assert_response :success
       assert_doesnt_have_login_form
       assert_select 'h1', :text => /Tim Diggins/
+      assert_have_new_project_link false
+    end
+    should "be able to see self with links" do
+      get("users/alex")
+      assert_response :success
+      assert_doesnt_have_login_form
+      assert_select 'h1', :text => /Alexander Kohlhofer/
+      assert_have_new_project_link true
     end
     
   end
   
+  def assert_have_new_project_link has_link
+    assert_select '#new-project-link', :count=>has_link ? 1 : 0      
+  end
 end
