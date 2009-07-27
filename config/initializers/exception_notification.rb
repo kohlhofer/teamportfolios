048c806f57fce94423d4fcf476517fce79e48a2c
Tim Diggins <tim@red56.co.uk>
2009-07-27 12:18:00 +0100

    trying to add exception notification (needs testing)

---
+++ config/initializers/exception_notification.rb
@@ -0,0 +1,3 @@
+ExceptionNotifier.exception_recipients = %w(alexander@kohlhofer.com tim@red56.co.uk)
+ExceptionNotifier.sender_address = "Team Porfolios Application Error <do-not-respond@teamportfolios.com>"
+ExceptionNotifier.email_prefix = "[* Team Portfolios Error *] "
