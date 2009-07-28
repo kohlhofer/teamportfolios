4c6c5061219649338b40fca1551190331d358511
Tim Diggins <tim@red56.co.uk>
2009-07-28 20:37:44 +0100

    temporarily turning off actually sending emails

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -8,17 +8,17 @@ class Notification < ActiveRecord::Base
   end
   
   def process
-    begin
-      NotificationMailer.send("deliver_#{self.action}".to_sym, self.email_address)
-    rescue Exception => e
-      self.attempted_send = Time.now
-      self.failure_msg = e.to_s
-      puts "# Attempted to send '#{self.action}' to #{self.email_address.email}"
-      puts "#           but '#{e}'"
-      save
-      return
-    end 
+#    begin
+#      NotificationMailer.send("deliver_#{self.action}".to_sym, self.email_address)
+#    rescue Exception => e
+#      self.attempted_send = Time.now
+#      self.failure_msg = e.to_s
+#      puts "# Attempted to send '#{self.action}' to #{self.email_address.email}"
+#      puts "#           but '#{e}'"
+#      save
+#      return
+#    end 
     puts "# sent '#{self.action}' to #{self.email_address.email}"
-    self.destroy
+#    self.destroy
   end
 end
